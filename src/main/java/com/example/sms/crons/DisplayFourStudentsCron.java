d3e5590068974b7e801af001d2c9b2b447b60724
morris-wambua <morriswambua7@gmail.com>
2022-01-24 20:16:39 +0300

    modify cron

---
+++ src/main/java/com/example/sms/crons/DisplayFourStudentsCron.java
@@ -0,0 +1,39 @@
+package com.example.sms.crons;
+
+import com.example.sms.entity.Student;
+import com.example.sms.repositories.StudentRepository;
+import com.example.sms.services.StudentService;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@EnableScheduling
+public class DisplayFourStudentsCron {
+
+    private StudentService studentService;
+
+    public DisplayFourStudentsCron(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @Scheduled(cron="1 * * * * *")
+    public void displayFourStudents(){
+
+        System.out.println("Started the cron");
+        studentService.getFourStudents();
+        System.out.println("Complete");
+    }
+
+    @Scheduled(cron="1 * * * * *")
+    public void displayRestStudents(){
+
+        System.out.println("Started the cron2");
+        studentService.backUpDB();
+        System.out.println("Complete cron 2");
+    }
+
+    }
+
